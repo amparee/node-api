3285b0e5508156bb40b6c9569418969a34fed188
Pablo Servile <pablo.mcd@hotmail.com>
2019-06-05 15:18:21 -0300

    added artillery

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,8 +5,10 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+var contador = 0;
 app.get('/hola', function(req, res) {
+    contador++;
+    console.log("GET - " + contador)
     res.json('[GET]Usuario local');
 });
 
